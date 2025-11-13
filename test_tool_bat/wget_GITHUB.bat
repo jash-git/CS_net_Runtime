2f7098e77f5891f8d17453b3b912851e04675936
Jash.Liao <jash.liao@gmail.com>
2025-11-13 16:11:42 +0800

    Modieid 202511131611

--- test_tool_bat/wget_GITHUB.bat
+++ test_tool_bat/wget_GITHUB.bat
@@ -1,6 +1,6 @@
 REM https://github.com/jash-git/CS_net_Runtime/raw/refs/heads/main/windowsdesktop-runtime-8.0.12-win-x64.exe
 REM raw/refs/heads/main/ -> raw/main/
 
-wget --no-check-certificate https://github.com/jash-git/CS_net_Runtime/raw/main/windowsdesktop-runtime-8.0.12-win-x64.exe
+wget --no-check-certificate https://github.com/jash-git/CS_net_Runtime/raw/main/windowsdesktop-runtime-10.0.0-win-x86.exe
 
 
